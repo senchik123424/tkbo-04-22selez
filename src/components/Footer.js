9f600d225c425380658fc2684f2b057f53d057bf
merlooy <k.akbarova.k@mail.ru>
2024-04-11 12:39:09 +0300

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer className="page-footer teal lighten-1">
+            <div className="container">
+                © {new Date().getFullYear()} Акбарова Карина
+            </div>
+        </footer>
+    );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+export default function Header(props) {
+    return (
+        <header>
+            <nav className="teal lighten-1">
+                <div className="nav-wrapper container">
+                    <a href="#" className="brand-logo">Fortnite skins</a>
+                    <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    );
+}
